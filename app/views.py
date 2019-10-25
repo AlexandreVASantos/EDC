7b1ba3b96c8bce2ee2087b546a38e56c27c391d6
Alexandre <alexandresantos1@ua.pt>
2019-10-25 12:23:47 +0100

    still doing edit function, comparing values to edit

--- app/views.py
+++ app/views.py
@@ -102,6 +102,8 @@ def edit_recipe(request):
 
                 dict_categorias = xmltodict.parse(exec)
 
+                print(dict_categorias)
+
 
                 lista_categorias = ""
                 if (len(dict_categorias["categorias"]) > 1):
@@ -135,7 +137,7 @@ def edit_recipe(request):
                 dict_passos = xmltodict.parse(exec)
                 lista_passos=""
                 for passo in dict_passos["descriçao"]["descriçao"]["passo"]:
-                    lista_passos+= passo + ","
+                    lista_passos+= passo + "\n"
 
                 q = session.query(
                     'import module namespace funcs = "com.funcs.my.index" at "index.xqm";funcs:get_ingredientes_receita("{}")'.format(
@@ -151,12 +153,12 @@ def edit_recipe(request):
                 for item in dict_ingredientes["ingredientes"]["ingrediente"]:
                     item = list(item.items())
                     if len(item) == 3:
-                        lista_ingredientes+= item[2][1] + ","+ item[0][1] +"," + item[1][1]+ ","
+                        lista_ingredientes+= item[2][1] + ","+ item[0][1] +"," + item[1][1]+ ",\n"
                     else:
-                        lista_ingredientes += item[1][1] + "," + item[0][1] + ","
+                        lista_ingredientes += item[1][1] + "," + item[0][1] + ",\n"
 
+                print(lista_passos)
 
-                print(lista_ingredientes)
 
             finally:
                 # close session
@@ -166,8 +168,141 @@ def edit_recipe(request):
 
 
             return JsonResponse({"receita": [dict_recipe["receita"]["nome"],lista_categorias,dict_recipe["receita"]["data"],lista_tipos,lista_autores,dict_recipe["receita"]["dificuldade"],lista_ingredientes,lista_passos,dict_recipe["receita"]["imagem"]]})
+        else:
+            nome_receita = request.POST.get("receitas")
 
-        return render(request)
+            session = BaseXClient.Session('localhost', 1984, 'admin', 'admin')
+            try:
+                q = session.query(
+                    'import module namespace funcs = "com.funcs.my.index" at "index.xqm";funcs:get_receita("{}")'.format(
+                        nome_receita))
+                exec = q.execute()
+                q.close()
+
+                dict_recipe = xmltodict.parse(exec)
+
+                next_nome_receita= request.POST.get("nome")
+
+                if dict_recipe["receita"]["nome"] != nome_receita or  dict_recipe["receita"]["data"] != request.POST.get("data") or  dict_recipe["receita"]["dificuldade"] != request.POST.get("dificuldade") or  dict_recipe["receita"]["imagem"] != request.POST.get("imagem"):
+                    q = session.query(
+                        'import module namespace funcs = "com.funcs.my.index" at "index.xqm";funcs:update_receita("{}", "{}", "{}","{}","{}")'.format(
+                            nome_receita, next_nome_receita, request.POST.get("dificuldade"), request.POST.get("imagem"),request.POST.get("data") ))
+                    exec = q.execute()
+                    q.close()
+
+
+
+                q = session.query(
+                    'import module namespace funcs = "com.funcs.my.index" at "index.xqm";funcs:get_autores_receita("{}")'.format(
+                        next_nome_receita))
+                exec = q.execute()
+                q.close()
+
+                dict_autores = xmltodict.parse(exec)
+                list_autores_original=dict_autores["autores"]["autor"]
+
+                new_list_autores = request.POST.get("auth").split(",")
+
+
+                for auth in new_list_autores:
+                    if auth not in list_autores_original:
+                        q = session.query(
+                            'import module namespace funcs = "com.funcs.my.index" at "index.xqm";funcs:add_autor("{}", "{}")'.format(
+                                next_nome_receita, auth))
+                        q.execute()
+                        q.close()
+
+                for auth in list_autores_original:
+                    if auth not in new_list_autores:
+                        q = session.query(
+                            'import module namespace funcs = "com.funcs.my.index" at "index.xqm";funcs:delete_autor("{}", "{}")'.format(
+                                next_nome_receita, auth))
+                        q.execute()
+                        q.close()
+
+                q = session.query(
+                    'import module namespace funcs = "com.funcs.my.index" at "index.xqm";funcs:get_categorias_receita("{}")'.format(
+                        next_nome_receita))
+                exec = q.execute()
+                q.close()
+
+                dict_categorias = xmltodict.parse(exec)
+
+                list_categorias_original = dict_categorias["categorias"]["categoria"]
+
+                new_list_categorias =  request.POST.get("cat").split(",")
+
+
+                for cat in new_list_categorias:
+                    if cat not in list_categorias_original:
+                        q = session.query(
+                            'import module namespace funcs = "com.funcs.my.index" at "index.xqm";funcs:add_autor("{}", "{}")'.format(
+                                next_nome_receita, cat))
+                        q.execute()
+                        q.close()
+
+                for cat in list_categorias_original:
+                    if cat not in new_list_categorias:
+                        q = session.query(
+                            'import module namespace funcs = "com.funcs.my.index" at "index.xqm";funcs:delete_autor("{}", "{}")'.format(
+                                next_nome_receita, cat))
+                        q.execute()
+                        q.close()
+
+                q = session.query(
+                    'import module namespace funcs = "com.funcs.my.index" at "index.xqm";funcs:get_tipos_receita("{}")'.format(
+                        request.POST.get("selected_recipe")))
+                exec = q.execute()
+                q.close()
+
+                dict_tipos = xmltodict.parse(exec)
+
+                lista_tipos = ""
+
+                if (len(dict_tipos["tipos"]) > 1):
+                    for tipo in dict_tipos["tipos"]["tipo"]:
+                        lista_tipos += tipo + ","
+                else:
+                    lista_tipos = dict_tipos["tipos"]["tipo"]
+
+                q = session.query(
+                    'import module namespace funcs = "com.funcs.my.index" at "index.xqm";funcs:get_passos_receita("{}")'.format(
+                        request.POST.get("selected_recipe")))
+                exec = q.execute()
+                q.close()
+
+                dict_passos = xmltodict.parse(exec)
+                lista_passos = ""
+                for passo in dict_passos["descriçao"]["descriçao"]["passo"]:
+                    lista_passos += passo + "\n"
+
+                q = session.query(
+                    'import module namespace funcs = "com.funcs.my.index" at "index.xqm";funcs:get_ingredientes_receita("{}")'.format(
+                        request.POST.get("selected_recipe")))
+                exec = q.execute()
+                q.close()
+
+                dict_ingredientes = xmltodict.parse(exec)
+                lista_ingredientes = ""
+
+                for item in dict_ingredientes["ingredientes"]["ingrediente"]:
+                    item = list(item.items())
+                    if len(item) == 3:
+                        lista_ingredientes += item[2][1] + "," + item[0][1] + "," + item[1][1] + ",\n"
+                    else:
+                        lista_ingredientes += item[1][1] + "," + item[0][1] + ",\n"
+
+                print(lista_passos)
+
+
+            finally:
+                # close session
+                if session:
+                    session.close()
+
+            return render(request)
+    else:
+        return edit_receita(request)
 
 
 
