b17fe099547a50e1f4a88d0c19487f67aed2a6f1
Alexandre <alexandresantos1@ua.pt>
2019-12-10 17:28:31 +0000

    delete done and apply filters too

--- projeto2/app/views.py
+++ projeto2/app/views.py
@@ -14,11 +14,62 @@ def home(request):
 
 
 def listrecipes(request):
-
     return render(request,"list_recipes.html", {"info":getInfoReceita(),"autores" : getAutores(), "categorias" : getCategorias(), "tags" : getTipos(), "dificuldade": getDificuldades()})
 
 def applyFilters(request):
-   return None
+    endpoint = "http://localhost:7200"
+    repo_name = "receitas"
+    client = ApiClient(endpoint=endpoint)
+    accessor = GraphDBApi(client)
+
+    query = 'PREFIX aut:<http://receita/autores/pred/nome>'\
+            'PREFIX cat:<http://receita/categorias/pred/nome>'\
+            'PREFIX tip:<http://receita/tipos/pred/nome>'\
+            'PREFIX dif:<http://receita/dificuldades/pred/nome>'\
+            'PREFIX predRec:<http://receita/pred/>'\
+            'Select ?nome ?imagem ?autor where{'\
+            '?id predRec:nome ?nome.'\
+            '?id predRec:imagem ?imagem.' \
+            '?id predRec:autor ?aut_id.'\
+            '?aut_id aut: ?autor.'\
+
+
+
+    if request.GET.get("authors") != "None":
+        query += ' ?aut_id aut: "'+ str(request.GET.get("authors"))+'". '
+
+    if request.GET.get("dificuldade") != "None":
+        query += '?id predRec:dificuldade ?id_d. ?dif_id dif: "' + str(request.GET.get("dificuldade")) + '". '
+
+    if request.GET.get("categorias") != "None":
+        query += '?id predRec:categoria ?cat_id. ?cat_id cat: "' + str(request.GET.get("categorias")) + '". '
+
+    if request.GET.get("tags") != "None":
+        query += '?id predRec:tipo ?id_t. ?id_t tip: "' + str(request.GET.get("tags")) + '". '
+
+    query += '}order by asc(UCASE(str(?nome)))'
+
+    print(query)
+    payload_query = {"query": query}
+    res = accessor.sparql_select(body=payload_query, repo_name=repo_name)
+    res = json.loads(res)
+    info = {}
+
+    for rec in res["results"]["bindings"]:
+        auth_list = []
+        print(rec)
+        info[rec["nome"]["value"]] = []
+        info[rec["nome"]["value"]].append(rec["imagem"]["value"])
+        if isinstance(rec["autor"]["value"], list):
+            for auth in rec["autor"]["value"]:
+                auth_list.append(auth)
+        else:
+            auth_list.append(rec["autor"]["value"])
+
+
+        info[rec["nome"]["value"]].append(auth_list)
+
+    return render(request, "list_recipes.html", {"info":info,"autores" : getAutores(), "categorias" : getCategorias(), "tags" : getTipos(), "dificuldade": getDificuldades()})
 
 
 
@@ -537,10 +588,34 @@ def add_recipe(request):
 
 
 def delete(request):
-    return None
+    receitas = getNomesReceitas()
+    return render(request,"del.html", {"receitas":receitas,"delete_occurs" : False})
 
 def del_recipe(request):
-    return None
+    try:
+        delete_occurs=True
+        error = False
+        receita = request.POST.get("receitas")
+        endpoint = "http://localhost:7200"
+        repo_name = "receitas"
+        client = ApiClient(endpoint=endpoint)
+        accessor = GraphDBApi(client)
+
+
+        query = 'PREFIX predRec:<http://receita/pred/>' \
+                ' Delete {?id_r ?p ?o} where{' \
+                '?id_r predRec:nome "' + str(receita) +'".' \
+                '?id_r ?p ?o.'\
+                '}'
+        payload_query = {"update": query}
+        res = accessor.sparql_update(body=payload_query, repo_name=repo_name)
+
+    except:
+        error = True
+
+    receitas = getNomesReceitas()
+
+    return render(request, "del.html", {"receitas":receitas,"delete_occurs":delete_occurs, "error":error})
 
 def show_recipe(request, recipe):
     return None
@@ -662,7 +737,7 @@ def getInfoReceita():
     accessor = GraphDBApi(client)
     receitas_info = {}
     for rec in receitas:
-        print(rec)
+
         query = 'PREFIX predRec:<http://receita/pred/>' \
                 'Select ?imagem where{' \
                 ' ?r predRec:nome "' + rec + '".' \
@@ -676,19 +751,25 @@ def getInfoReceita():
         receitas_info[rec].append(res["results"]["bindings"][0]["imagem"]["value"])
 
         query = 'PREFIX predRec:<http://receita/pred/>'\
-                'PREFIX aut:<http://receita/autores/pred/nome/>'\
+                'PREFIX aut:<http://receita/autores/pred/nome>'\
                 'Select ?autor where{'\
-                '?r predRec:nome "'+ rec +'".'\
+                '?r predRec:nome "'+ str(rec) +'".'\
                 '?r predRec:autor ?id_a.'\
                 '?id_a aut: ?autor.'\
                 '}'
+
         payload_query = {"query": query}
         res = accessor.sparql_select(body=payload_query, repo_name=repo_name)
         res = json.loads(res)
         autores_rec=[]
 
-        for autor in res["results"]["bindings"]:
-            autores_rec.append(autor["autor"]["value"])
+
+
+        if isinstance(res["results"]["bindings"], list):
+            for auth in res["results"]["bindings"]:
+                autores_rec.append(auth["autor"]["value"])
+        else:
+            autores_rec.append(res["results"]["bindings"]["autor"]["value"])
 
         receitas_info[rec].append(autores_rec)
 
